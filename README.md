a30ebabaaeef31f69f4792a240db2d84a4614357
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-02-07 20:06:09 +0100

    Initial release

---
+++ README.md
@@ -0,0 +1,9 @@
+# Creative One-Punch
+
+A Fabric Minecraft mod which lets you One-Punch any entity while in creative mode, as long as you don't hold a weapon.
+
+![Logo](src/main/resources/assets/creativeonepunch/icon.png)
+
+* requires Minecraft 1.16.2+, Fabric Loader 0.11.1+ and Fabric API
+* works server-only
+* works in single player when installed on the client
---
+++ src/main/java/fourmisain/creativeonepunch/CreativeOnePunch.java
@@ -0,0 +1,27 @@
+package fourmisain.creativeonepunch;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.*;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.util.ActionResult;
+
+public class CreativeOnePunch implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+			if (player.isCreative()) {
+				// get main hand item attack damage
+				double attackDamage = 0;
+				for (EntityAttributeModifier modifier : player.getMainHandStack().getAttributeModifiers(EquipmentSlot.MAINHAND).get(EntityAttributes.GENERIC_ATTACK_DAMAGE))
+					attackDamage += modifier.getValue();
+
+				if (attackDamage == 0)
+					entity.damage(DamageSource.player(player), 9999);
+			}
+
+			return ActionResult.PASS;
+		});
+	}
+}
