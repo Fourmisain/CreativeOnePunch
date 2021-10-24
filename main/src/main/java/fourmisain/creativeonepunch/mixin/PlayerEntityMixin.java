ea555105d724cee2c78fd28f0c45ce328aa5bd02
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-10-24 23:07:39 +0200

    experimental build

---
+++ main/src/main/java/fourmisain/creativeonepunch/mixin/PlayerEntityMixin.java
@@ -0,0 +1,35 @@
+package fourmisain.creativeonepunch.mixin;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+import static net.minecraft.entity.attribute.EntityAttributeModifier.Operation.ADDITION;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
+	@ModifyArgs(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
+	public void onePunch(Args args) {
+		PlayerEntity player = (PlayerEntity) (Object) this;
+
+		if (player.isCreative()) {
+			// get main hand item attack damage (assuming multiplier is never 0)
+			double attackDamage = 0.0;
+			for (EntityAttributeModifier modifier : player.getMainHandStack().getAttributeModifiers(EquipmentSlot.MAINHAND).get(EntityAttributes.GENERIC_ATTACK_DAMAGE)) {
+				if (modifier.getOperation() == ADDITION) {
+					attackDamage += modifier.getValue();
+				}
+			}
+
+			if (attackDamage == 0.0) {
+				// One Punch!
+				args.set(1, 9999f);
+			}
+		}
+	}
+}
