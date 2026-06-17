a2922f01575428e434e4452f64599e0719083e4e
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2026-06-17 11:51:09 +0200

    reduce damage for sulfur cubes so they don't leave the stratosphere

--- 26_1/src/main/java/io/github/fourmisain/creativeonepunch/mixin/PlayerMixin.java
+++ 26_1/src/main/java/io/github/fourmisain/creativeonepunch/mixin/PlayerMixin.java
@@ -1,5 +1,8 @@
 package io.github.fourmisain.creativeonepunch.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import org.apache.commons.lang3.mutable.MutableDouble;
@@ -13,7 +16,7 @@ import static net.minecraft.world.entity.ai.attributes.AttributeModifier.Operati
 @Mixin(Player.class)
 public abstract class PlayerMixin {
 	@ModifyArg(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;hurtOrSimulate(Lnet/minecraft/world/damagesource/DamageSource;F)Z"), index = 1)
-	public float onePunch(float originalDamage) {
+	public float onePunch(float originalDamage, @Local(argsOnly = true) Entity entity) {
 		Player player = (Player) (Object) this;
 
 		if (player.isCreative()) {
@@ -27,6 +30,9 @@ public abstract class PlayerMixin {
 			});
 
 			if (attackDamage.doubleValue() == 0.0) {
+				if (entity.typeHolder().is(Identifier.withDefaultNamespace("sulfur_cube")))
+					return 99f;
+
 				// One Punch!
 				return 9999f;
 			}
